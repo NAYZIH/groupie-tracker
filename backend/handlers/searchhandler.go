66c2e3ade13092698883723d2163c17cb4610124
msrirams <sriramsourya.mallipoudy@ynov.com>
2025-02-10 13:06:35 +0100

    scripts and search bar update

--- backend/handlers/searchhandler.go
+++ backend/handlers/searchhandler.go
@@ -29,6 +29,7 @@ func SearchHandler(w http.ResponseWriter, r *http.Request) {
 				"url":   fmt.Sprintf("/info/%d", artist.ID),
 			})
 		}
+
 		for _, member := range artist.Members {
 			if strings.Contains(strings.ToLower(member), strings.ToLower(query)) {
 				results = append(results, map[string]string{
@@ -37,6 +38,27 @@ func SearchHandler(w http.ResponseWriter, r *http.Request) {
 					"url":   fmt.Sprintf("/info/%d", artist.ID),
 				})
 			}
+		}
+		if strings.Contains(strings.ToLower(artist.Locations), strings.ToLower(query)) {
+			results = append(results, map[string]string{
+				"label": artist.Locations,
+				"type":  "location",
+				"url":   fmt.Sprintf("/info/%d", artist.ID),
+			})
+		}
+		if strings.Contains(strings.ToLower(artist.FirstAlbum), strings.ToLower(query)) {
+			results = append(results, map[string]string{
+				"label": artist.FirstAlbum,
+				"type":  "first album",
+				"url":   fmt.Sprintf("/info/%d", artist.ID),
+			})
+		}
+		if strings.Contains(fmt.Sprintf("%d", artist.CreationDate), query) {
+			results = append(results, map[string]string{
+				"label": fmt.Sprintf("Année de création: %d", artist.CreationDate),
+				"type":  "creation date",
+				"url":   fmt.Sprintf("/info/%d", artist.ID),
+			})
 		}
 	}
 
